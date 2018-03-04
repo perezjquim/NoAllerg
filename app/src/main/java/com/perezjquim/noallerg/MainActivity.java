041d879fb549c29c8cc1be469c5b93a86c72eb8d
Joaquim Perez <jquim1@hotmail.com>
2018-03-04 22:35:11 +0000

    Splash screen and simple map

--- app/src/main/java/com/perezjquim/noallerg/MainActivity.java
+++ app/src/main/java/com/perezjquim/noallerg/MainActivity.java
@@ -1,13 +1,39 @@
 package com.perezjquim.noallerg;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.views.MapView;
+
+public class MainActivity extends AppCompatActivity
+{
+    MapView mapView=null;
+    @Override
+    public void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        Configuration.getInstance().load(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        //TODO check permissions
+        setContentView(R.layout.activity_main);
+        mapView = (MapView) findViewById(R.id.map);
+    }
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onResume(){
+        super.onResume();
+        Configuration.getInstance().load(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        if (mapView!=null)
+            mapView.onResume();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Configuration.getInstance().save(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        if (mapView!=null)
+            mapView.onPause();
     }
 }
