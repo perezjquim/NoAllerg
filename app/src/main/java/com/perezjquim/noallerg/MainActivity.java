b8fd67c52f9a5fd8b50c856c02d116463e0b53a6
Joaquim Perez <jquim1@hotmail.com>
2018-03-05 17:50:00 +0000

    Lower sdk allowed

--- app/src/main/java/com/perezjquim/noallerg/MainActivity.java
+++ app/src/main/java/com/perezjquim/noallerg/MainActivity.java
@@ -1,6 +1,5 @@
 package com.perezjquim.noallerg;
 
-
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -10,30 +9,35 @@ import org.osmdroid.views.MapView;
 
 public class MainActivity extends AppCompatActivity
 {
-    MapView mapView=null;
+    MapView map =null;
     @Override
-    public void onCreate(Bundle savedInstance){
+    public void onCreate(Bundle savedInstance)
+    {
         super.onCreate(savedInstance);
         Configuration.getInstance().load(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
         //TODO check permissions
         setContentView(R.layout.activity_main);
-        mapView = (MapView) findViewById(R.id.map);
+        map = (MapView) findViewById(R.id.map);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
     }
 
 
     @Override
-    public void onResume(){
+    public void onResume()
+    {
         super.onResume();
         Configuration.getInstance().load(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
-        if (mapView!=null)
-            mapView.onResume();
+        if (map !=null)
+            map.onResume();
     }
 
     @Override
-    public void onPause(){
+    public void onPause()
+    {
         super.onPause();
         Configuration.getInstance().save(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
-        if (mapView!=null)
-            mapView.onPause();
+        if (map !=null)
+            map.onPause();
     }
 }
