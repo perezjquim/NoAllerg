81edc0bd16ddd671386716d797fa59a2f5a12ad7
Joaquim Perez <jquim1@hotmail.com>
2018-03-11 14:59:44 +0000

    'admin' module added (WebView of its webpage)

---
+++ admin/src/main/java/com/perezjquim/noallerg/admin/MainActivity.java
@@ -0,0 +1,23 @@
+package com.perezjquim.noallerg.admin;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        getWindow().requestFeature(Window.FEATURE_PROGRESS);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView web = findViewById(R.id.web);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.loadUrl("http://noallerg.x10host.com/admin.php");
+    }
+}
